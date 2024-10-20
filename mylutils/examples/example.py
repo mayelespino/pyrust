c97c412a92ea659881286063419ea511062091ee
mayel espino <mayel.espino@gmail.com>
2024-10-20 04:29:34 +0000

    Added mylutils.read_proc_stat_cpu()

--- mylutils/examples/example.py
+++ mylutils/examples/example.py
@@ -1,4 +1,45 @@
 import mylutils
 
-for line in mylutils.read_txt("test.txt"):
-    print(line)
+#print("\nmylutils.read_txt(test.txt)\n")
+
+#for line in mylutils.read_txt("test.txt"):
+#    print(line)
+
+#print("\nmylutils.read_csv(test.csv)\n")
+#for line in mylutils.read_csv("test.csv"):
+#    for col in line:
+#        print(col,end=",")
+#    print()
+
+
+print("\nmylutils.read_proc_stat_cpu()\n")
+# print(mylutils.read_proc_stat_cpu())
+cpu_stats = mylutils.read_proc_stat_cpu()
+
+total_time = cpu_stats['total_time']
+user_time = cpu_stats['user_time']
+nice_time = cpu_stats['nice_time']
+system_time = cpu_stats['system_time']
+idle_time = cpu_stats['idle_time']
+iowait_time = cpu_stats['iowait_time']
+irq_time = cpu_stats['irq_time']
+softirq_time = cpu_stats['softirq_time']
+iowait_time = cpu_stats['iowait_time']
+
+print("total_time", total_time)
+print("")
+print("user_time", user_time)
+print("nice_time", nice_time)
+print("system_time", system_time)
+print("idle_time", idle_time)
+print("iowait_time", iowait_time)
+print("irq_time", irq_time)
+print("softirq_time", softirq_time)
+print("")
+print("user_percent", (user_time/total_time)*100)
+print("nice_percent", (nice_time/total_time)*100)
+print("system_percent", (system_time/total_time)*100)
+print("idle_percent", (idle_time/total_time)*100)
+print("iowait_percent", (iowait_time/total_time)*100)
+print("irq_percent", (irq_time/total_time)*100)
+print("softirq_percent", (softirq_time/total_time)*100)
--- mylutils/src/lib.rs
+++ mylutils/src/lib.rs
@@ -1,19 +1,102 @@
 use pyo3::prelude::*;
 use std::fs;
-
+use std::collections::HashMap;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn read_txt<'a>(file_path: &str) -> Vec<String> {
     let lines = fs::read_to_string(file_path)
         .expect("Should have been able to read the file.");
-    // vec![lines.lines().collect()]
     lines.lines().map(str::to_string).collect()
 }
+
+#[pyfunction]
+fn read_csv<'a>(file_path: &str) -> Vec<Vec<String>> {
+    let lines = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file.");
+    let lines_vec: Vec<String> = lines.lines().map(str::to_string).collect();
+    let lines_vec_iter = lines_vec.iter();
+    let mut result: Vec<Vec<String>> = Vec::new();
+    for line in lines_vec_iter {
+        let cols: Vec<String> = line.split(",").map(str::to_string).collect();
+        result.push(cols);
+    }
+    result
+}
+
+
+// https://www.linuxhowtos.org/System/procstat.htm
+//
+// Various pieces of information about kernel activity are available in the
+// /proc/stat file.
+// All of the numbers reported in this file are aggregates since the system first booted.
+// For a quick look, simply cat the file:
+
+// > cat /proc/stat
+// cpu  2255 34 2290 22625563 6290 127 456
+// cpu0 1132 34 1441 11311718 3675 127 438
+// cpu1 1123 0 849 11313845 2614 0 18
+// intr 114930548 113199788 3 0 5 263 0 4 [... lots more numbers ...]
+// ctxt 1990473
+// btime 1062191376
+// processes 2915
+// procs_running 1
+// procs_blocked 0
+// The very first "cpu" line aggregates the numbers in all of the other "cpuN" lines.
+
+// These numbers identify the amount of time the CPU has spent performing different kinds of work. Time units are in USER_HZ or Jiffies (typically hundredths of a second).
+
+// The meanings of the columns are as follows, from left to right:
+
+// user: normal processes executing in user mode
+// nice: niced processes executing in user mode
+// system: processes executing in kernel mode
+// idle: twiddling thumbs
+// iowait: waiting for I/O to complete
+// irq: servicing interrupts
+// softirq: servicing softirqs
+// The "intr" line gives counts of interrupts serviced since boot time, for each
+// of the possible system interrupts. The first column is the total of all interrupts serviced; each subsequent column is the total for that particular interrupt.
+
+#[pyfunction]
+fn read_proc_stat_cpu<'a>() -> HashMap<String, i32> {
+
+    let mut proc_file_lines = Vec::new();
+    for line in fs::read_to_string("/proc/stat").unwrap().lines() {
+        proc_file_lines.push(line.to_string())
+    }
+
+    let cols = proc_file_lines[0].split_whitespace().collect::<Vec<&str>>();
+    
+    let user = cols[1].parse::<i32>().unwrap();
+    let nice = cols[2].parse::<i32>().unwrap();
+    let system = cols[3].parse::<i32>().unwrap();
+    let idle = cols[4].parse::<i32>().unwrap();
+    let iowait = cols[5].parse::<i32>().unwrap();
+    let irq = cols[6].parse::<i32>().unwrap();
+    let softirq = cols[7].parse::<i32>().unwrap();
+    let total = user + nice + system + idle + iowait + irq + softirq;
+
+    let mut cpu_stats = HashMap::new();
+    cpu_stats.insert(String::from("total_time"),total);
+    cpu_stats.insert(String::from("system_time"),system);
+    cpu_stats.insert(String::from("user_time"), user);
+    cpu_stats.insert(String::from("idle_time"), idle);
+    cpu_stats.insert(String::from("nice_time"), nice);
+    cpu_stats.insert(String::from("iowait_time"), iowait);
+    cpu_stats.insert(String::from("irq_time"), irq);
+    cpu_stats.insert(String::from("softirq_time"), softirq);
+
+    cpu_stats
+}
+
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn mylutils(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_txt, m)?)?;
+    m.add_function(wrap_pyfunction!(read_csv, m)?)?;
+    m.add_function(wrap_pyfunction!(read_proc_stat_cpu, m)?)?;
     Ok(())
 }
